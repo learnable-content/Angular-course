f7fad59431b78ba203582ef690e9713babcbc4c2
Rudolf Olah <omouse@gmail.com>
2016-12-24 11:44:09 -0500

    splitting between dev and production modes

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,46 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 var DebugWebpackPlugin = require('debug-webpack-plugin');
 var path = require('path');
 
+let isProduction = process.env.NODE_ENV === 'production';
+
+let plugins = [
+  new webpack.DefinePlugin({
+    PRODUCTION: JSON.stringify(isProduction)
+  }),
+  new HtmlWebpackPlugin({
+    template: 'index.html',
+    chunks: ['vendor', 'app']
+  }),
+  new CopyWebpackPlugin([
+    {
+      from: 'assets',
+      to: 'assets'
+    }
+  ])
+];
+
+if (isProduction) {
+  plugins = plugins.concat([
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      comments: false,
+      sourceMap: false
+    }),
+  ]);
+} else {
+  plugins = plugins.concat([
+    new DebugWebpackPlugin({
+      debug: true
+    })
+  ]);
+}
+
 module.exports = {
-  //devtool: 'source-map',
+  devtool: 'cheap-source-map',
   entry: {
+    'vendor': ['./app/polyfills.ts', './app/vendor.ts'],
     'app': './app/main.ts'
   },
   module: {
@@ -43,26 +80,7 @@ module.exports = {
     filename: '[name].js',
     path: './dist/'
   },
-  plugins: [
-    /*new DebugWebpackPlugin({
-      debug: true
-    }),*/
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      mangle: false
-    }),
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: 'assets',
-        to: 'assets'
-      }
-    ])
-  ],
+  plugins: plugins,
   resolve: {
     extensions: ['', '.ts', '.js'],
     moduleDirectories: ['node_modules'],
