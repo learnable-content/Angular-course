6f1ad96067c37978aba0d4a972c659d06b36b84d
Rudolf Olah <omouse@gmail.com>
2017-06-21 09:18:32 -0400

    start from lesson3.3_final

---
+++ app/shared/analytics.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from "@angular/core";
+import { Http, Response } from "@angular/http";
+
+@Injectable()
+export class AnalyticsService {
+    constructor (private http: Http) {
+    }
+
+    event() {
+    }
+
+    report() {
+    }
+};
