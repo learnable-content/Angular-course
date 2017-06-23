127002f316b677e027f3441802cd1e9ad7989d80
Rudolf Olah <omouse@gmail.com>
2017-06-23 08:29:35 -0400

    starting from lesson4.4

---
+++ app/ads/report.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "ads-report",
+    templateUrl: "./report.template.html",
+    styleUrls: ["./report.style.less"]
+})
+export class AdReportComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+};
