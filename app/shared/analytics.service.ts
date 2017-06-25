5d8955b5189e77c566881b1b66ecfc9e5264efd7
Rudolf Olah <omouse@gmail.com>
2017-06-25 19:09:47 -0400

    updated

--- app/shared/analytics.service.ts
+++ app/shared/analytics.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from "@angular/core";
 import { Http, Response } from "@angular/http";
 
-type EventType = "click";
-type EventTarget = "ad" | "event";
-
 @Injectable()
 export class AnalyticsService {
     constructor (private http: Http) {
     }
 
-    event(eventType: EventType, eventTarget: EventTarget, id: string) {
-        console.debug(`analytics logged event: ${eventType} - ${id}`);
+    event() {
+    }
+
+    report() {
     }
 };
