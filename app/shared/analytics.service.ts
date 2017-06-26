82ab3a9b26260b48bbcd1fff972e0d6e57354973
Rudolf Olah <omouse@gmail.com>
2017-06-25 22:20:16 -0400

    updated

--- app/shared/analytics.service.ts
+++ app/shared/analytics.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AnalyticsService {
-    constructor() {
+    constructor () {
     }
 
     event(eventType: any, eventTarget: any, id: string) {
