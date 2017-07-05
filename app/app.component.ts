d3254cfd712a6693573a204d26846be47854fcd2
Rudolf Olah <omouse@gmail.com>
2017-07-05 11:01:19 -0400

    updated from lesson6.3

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    templateUrl: "./app.template.html",
+    styleUrls: ["./app.style.less"]
+})
+export class AppComponent {
+};
---
+++ app/examples/example.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    templateUrl: "./example.template.html"
+})
+export class ExampleComponent {
+    constructor() {
+    }
+};
