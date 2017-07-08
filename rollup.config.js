33178df91d71d616c7a94ad589275b7c52ea171f
Rudolf Olah <omouse@gmail.com>
2017-07-08 09:52:59 -0400

    starts from 6.3_final

---
+++ rollup.config.js
@@ -0,0 +1,48 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import angular from 'rollup-plugin-angular';
+import uglify from 'rollup-plugin-uglify';
+
+const child_process = require('child_process');
+
+export default {
+    entry: 'app/main.ts',
+    dest: 'dist/build.js',
+    format: 'iife',
+    plugins: [
+        angular({
+            preprocessors: {
+                template: template => template,
+                style: lesscss => {
+                    return child_process.execSync('yarn run -s lessc -', {
+                        input: lesscss
+                    }).toString();
+                }
+            }
+        }),
+        typescript(),
+        nodeResolve({
+            jsnext: true,
+            main: true
+        }),
+        commonjs({
+            include: [
+                'node_modules/rxjs/**',
+                'node_modules/@swimlane/**'
+            ],
+            namedExports: {
+                'node_modules/@swimlane/ngx-datatable/release/index.js': [
+                    'NgxDatatableModule',
+                    'DatatableComponent'
+                ]
+            }
+        })
+    ],
+    onwarn: function(warning) {
+        if (warning.code === 'THIS_IS_UNDEFINED') {
+            return;
+        }
+        console.warn(warning.message);
+    }
+};
